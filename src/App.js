b3b4d35e94f45e1c74ff41f37b34510c47d1b78b
Eric Sung <eric@ericlsung.com>
2022-10-17 20:31:40 -0700

    updated to use wss

--- src/App.js
+++ src/App.js
@@ -6,16 +6,22 @@ import Web3 from 'web3';
 // import { BlockHeader, Block } from 'web3-eth' // ex. package types
 
 
-// Newest SDK from NPM - LulzNFT
+// New SDK from NPM - LulzNFT -> Prod
 import frame from 'conductive-sdk'
 frame.init(
-  'L9HJRU96BSSyC2xTP7vsf6shwjTEIzNuB4uBr8aXXBY',
-  {api_host:'https://analytics.qa-conductive.ai'}
+  'W08fDAFH3vBvxzn8LSfc7O8_7S9XtnZTyq0KTRnE6TQ',
+  {api_host:'https://analytics.conductive.ai'}
 )
+
+// New SDK from NPM -> QA site campaigns
+// frame.init(
+//   'L9HJRU96BSSyC2xTP7vsf6shwjTEIzNuB4uBr8aXXBY',
+//   {api_host:'https://analytics.qa-conductive.ai'}
+// )
 
 // Wallet connect function
 // const web3 = new Web3('ws://localhost:3001');
-const web3 = new Web3('ws://ericlsung.com/AstroChimp');
+const web3 = new Web3('wss://ericlsung.com/AstroChimp');
 async function connect() {
   if (window.ethereum) {
      await window.ethereum.request({ method: "eth_requestAccounts" });
@@ -36,21 +42,25 @@ function App() {
       <header className="App-header">
         <img id="NFT" src={logo} className="App-logo" alt="logo" />
         
-        <ul>
-          <li><a className="conductive-link" href="https://app.qa-conductive.ai" target="_blank">QA Conductive.ai</a></li>
-          <li><a className="conductive-link" href="https://qa.lulznft.com" target="_blank">QA LulzNFT</a></li>
-        </ul>
-
         <p id="goham">
           Testing NPM install of SDK 
           <br/>Pointed to QA analytics. 
           <br/>SDK-test project.
         </p>
     
+        <h2>QA Test</h2>
         <ul> 
-          <li><a href="https://qa.ape.to/m7dsz">Campaign link </a></li>
-          <li><a href="https://qa-discord.ape.to/PXyjsSEg8k">Discord Join </a></li>
+          <li><a href="https://qa.ape.to/m7dsz">QA Campaign link </a></li>
+          <li><a href="https://qa-discord.ape.to/PXyjsSEg8k">QA Discord Join </a></li>
         </ul>
+        
+        <h2>Prod Test</h2>
+        <ul> 
+          <li><a href="https://ape.to/jxtz9">Prod Campaign link </a></li>
+          <li><a href="https://discord.ape.to/3P9GyM2kzR">Prod Discord Join </a></li>
+        </ul>
+
+        
 
       {/* <input className="wconnect" type="button" value="Connect Wallet" onClick={connect}/> */}
  
